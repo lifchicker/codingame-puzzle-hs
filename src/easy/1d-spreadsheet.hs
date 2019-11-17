341670327f8c9879934db999506afc6065801c02
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2019-11-17 18:49:45 +0800

    Spreadsheet puzzle. First version. Working. Not enough optimized.

--- src/easy/1d-spreadsheet.hs
+++ src/easy/1d-spreadsheet.hs
@@ -1,29 +1,68 @@
+-- https://www.codingame.com/ide/puzzle/1d-spreadsheet
+
 import System.IO
 import Control.Monad
+import qualified Data.Map as M
+
+data Var = Val Int
+         | Ref Int
+         | None
+  deriving (Eq, Show)
+
+data Op = Value Var
+        | Add Var Var
+        | Sub Var Var
+        | Mult Var Var
+  deriving (Eq, Show)
+
+strToVar :: String -> Var
+strToVar s@(x:xs) = 
+  case x of '_' -> None
+            '$' -> Ref $ sToI xs
+            _ -> Val $ sToI s
+    where sToI y = read y :: Int
+
+strToOp :: String -> Var -> Var -> Op
+strToOp "VALUE" x None = Value x
+strToOp "ADD" x y = Add x y
+strToOp "SUB" x y = Sub x y
+strToOp "MULT" x y = Mult x y
+
+indexed :: [b] -> [(Int, b)]
+indexed = zipWith (,) [0..]
+
+lookupTable :: [a] -> M.Map Int a
+lookupTable e = M.fromList $ indexed e
+
+eval :: M.Map Int Op -> Var -> Int
+eval l (Val v) = v
+eval l (Ref r) = evaluate l (l M.! r)
+
+-- evaluate :: Op -> Int
+evaluate l (Value v) = eval l v
+evaluate l (Add x y) = (eval l x) + (eval l y)
+evaluate l (Sub x y) = (eval l x) - (eval l y)
+evaluate l (Mult x y) = (eval l x) * (eval l y)
+
+evalSpreadsheetCells :: [Op] -> [Int]
+evalSpreadsheetCells xs = map (evaluate l) xs
+  where l = lookupTable xs
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering -- DO NOT REMOVE
     
-  -- Auto-generated code below aims at helping you parse
-  -- the standard input according to the problem statement.
-    
   input_line <- getLine
   let n = read input_line :: Int
     
-  replicateM n $ do
+  cells <- replicateM n $ do
     input_line <- getLine
     let input = words input_line
-    let operation = input!!0
-    let arg1 = input!!1
-    let arg2 = input!!2
-    return ()
-    
-  replicateM n $ do
-        
-    -- hPutStrLn stderr "Debug messages..."
-        
-    -- Write answer to stdout
-    putStrLn "1"
-    return ()
+    let cell = strToOp (input!!0) (strToVar $ input!!1) (strToVar $ input!!2)
+    return (cell)
+  
+  hPutStrLn stderr (show cells)
+
+  mapM_ (putStrLn.show) (evalSpreadsheetCells cells)
+
   return ()
