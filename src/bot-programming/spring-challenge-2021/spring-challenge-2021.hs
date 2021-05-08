a3fc144c3e13fa28faf0695391b6b32c0ac3fc46
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2021-05-08 13:35:36 +0800

    Sping challenge 2021: wood 1 passed.

--- src/bot-programming/spring-challenge-2021/spring-challenge-2021.hs
+++ src/bot-programming/spring-challenge-2021/spring-challenge-2021.hs
@@ -2,72 +2,157 @@
 import Control.Monad
 import System.IO
 import Data.List
+import Data.Maybe (isJust, fromJust, listToMaybe)
 
 debug = hPutStrLn stderr
 
-data Richness = Low | Medium | High
-  deriving Show
+type Richness = Int
 
-toRichness :: Int -> Richness
-toRichness 1 = Low
-toRichness 2 = Medium
-toRichness 3 = High
-
-data Cell = Cell { cellIndex :: Int
-                 , cellRichness :: Richness
-                 , cellNeighbours :: [Int]
+data Cell = Cell { _index :: Int
+                 , _richness :: Richness
+                 , _neighbours :: [Int]
 } deriving Show
 
 type Forest = [Cell]
 
-newtype TreeSize = TreeSize Int
-  deriving Show
+type TreeSize = Int
 
-seed = TreeSize 0
+seed = 0 :: TreeSize
 
-data Tree = Tree { treeCellIndex :: Int
-                 , treeSize :: TreeSize
-                 , treeIsMine :: Bool
-                 , treeIsDormant :: Bool
+bigger :: TreeSize -> TreeSize
+bigger = succ
+
+data Tree = Tree { _cellIndex :: Int
+                 , _treeSize :: TreeSize
+                 , _isMine :: Bool
+                 , _isDormant :: Bool
 } deriving Show
 
-data Action = WAIT | COMPLETE Int
+data Action = WAIT
+            | COMPLETE Int
+            | GROW Int
+            | SEED Int Int
   deriving Show
 
 toAction :: [String] -> Action
 toAction ("WAIT" : _) =  WAIT
-toAction ("COMPLETE": xs) = COMPLETE (read (head xs) :: Int)
+toAction ("COMPLETE" : xs) = COMPLETE (read (head xs) :: Int)
+toAction ("GROW" : xs : _) = GROW (read xs :: Int)
+toAction ("SEED" : source : target : _) = SEED (read source :: Int) (read target :: Int)
+
+getIndex :: Action -> Int
+getIndex (COMPLETE i) = i
+getIndex (GROW i) = i
+getIndex _ = -1
 
 isComplete :: Action -> Bool
-isComplete c@(COMPLETE _) = True
+isComplete (COMPLETE _) = True
 isComplete _ = False
 
-data Turn = Turn { tDay :: Int
-                 , tNutrients :: Int
-                 , tMySun :: Int
-                 , tMyScore :: Int
-                 , tOppSun :: Int
-                 , tOppScore :: Int
-                 , tOppIsWaiting :: Bool
-                 , tTrees :: [Tree]
-                 , tPossibleActions :: [Action]
+isGrow :: Action -> Bool
+isGrow (GROW _) = True
+isGrow _ = False
+
+type Value = Int
+
+data Tradeoffs = Tradeoffs { _possibleAward :: Int
+                           , _currentCost :: Int
 } deriving Show
 
-pickComplete :: [Action] -> Maybe Action
-pickComplete = find isComplete
+data TreeInfo = TreeInfo { _tree :: Tree
+                         , _cell :: Cell
+                         , _action :: Maybe Action
+                         , _tradeoffs :: Tradeoffs
+} deriving Show
+
+data Turn = Turn { _day :: Int
+                 , _nutrients :: Int
+                 , _mySun :: Int
+                 , _myScore :: Int
+                 , _oppSun :: Int
+                 , _oppScore :: Int
+                 , _oppIsWaiting :: Bool
+                 , _trees :: [Tree]
+                 , _possibleActions :: [Action]
+} deriving Show
+
+---------------------------------------------------------------------------------------------------
+----------------------------------------------- AI ------------------------------------------------
+---------------------------------------------------------------------------------------------------
+
+possibleAward :: Int -> Int -> Int
+possibleAward nutrients richness = nutrients + richnessAward
+  where
+    richnessAward = case richness of
+      1 -> 0
+      2 -> 2
+      3 -> 4
+
+totalCost = undefined
+currentCost = undefined
+
+costToGrow :: [Tree] -> TreeSize -> Int
+costToGrow ts s = case s of
+      1 -> 3 + totalOwn
+      2 -> 7 + totalOwn
+      3 -> 0
+  where
+    totalOwn = length $ filter (\t -> _treeSize t == bigger s) ts
+
+
+myTrees :: [Tree] -> [Tree]
+myTrees = filter _isMine
+
+isActionable :: TreeInfo -> Bool
+isActionable (TreeInfo _ _ (Just a) _) = True
+isActionable _ = False
+
+getIndexes :: (Action -> Bool) -> [Action] -> [Int]
+getIndexes f as = map getIndex $ filter f as
 
 chooseAction :: Maybe Action -> Action
 chooseAction (Just a) = a
 chooseAction Nothing = WAIT
 
-doMagic :: IO ()
-doMagic = do
+getTreeInfo :: Forest -> Turn -> Tree -> TreeInfo
+getTreeInfo forest turn tree = TreeInfo tree cell action tradeoffs
+  where
+    treeIndex = _cellIndex tree
+    cell = fromJust $ find (\c -> _index c == treeIndex) forest
+    action = find (\a -> getIndex a == treeIndex) (_possibleActions turn)
+    tradeoffs = Tradeoffs award cost
+    award = possibleAward (_nutrients turn) (_richness cell)
+    cost = costToGrow (myTrees (_trees turn)) (_treeSize tree)
+
+goodnessScore :: TreeInfo -> Int
+goodnessScore (TreeInfo _ _ _ tradeoffs) = _possibleAward tradeoffs - _currentCost tradeoffs
+
+getAction (TreeInfo _ _ a _) = fromJust a
+
+candidateToAction (Just (s, info)) = getAction info
+candidateToAction Nothing = WAIT
+
+doMagic :: Forest -> IO ()
+doMagic forest = do
     turn <- readTurn
 
     debug (show turn)
 
-    let action = chooseAction $ pickComplete (tPossibleActions turn)
+    let mine = myTrees (_trees turn)
 
+    let treesWithInfo = map (getTreeInfo forest turn) mine
+
+    debug (show treesWithInfo)
+
+    let scoreWithInfo = map (\i -> (goodnessScore i, i)) (filter isActionable treesWithInfo)
+    let sortedByScore = sortBy (\(s1, i1) (s2, i2) -> compare s2 s1) scoreWithInfo
+    debug (show sortedByScore)
+
+    let candidate = listToMaybe sortedByScore
+
+    debug (show candidate)
+
+    let action = candidateToAction candidate
     -- hPutStrLn stderr "Debug messages..."
 
     -- GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
@@ -88,7 +173,7 @@ main = do
   cells <- readCells (read input_line :: Int)
 
   -- game loop
-  forever doMagic
+  forever $ do doMagic cells
 
 readTurn = do
   input_line <- getLine
@@ -113,7 +198,7 @@ readTurn = do
   return (Turn day nutrients sun score oppsun oppscore oppiswaiting trees actions)
 
 
-treeFromString input_line = Tree cellindex (TreeSize size) ismine isdormant
+treeFromString input_line = Tree cellindex size ismine isdormant
   where
     input = words input_line
     cellindex = read (input !! 0) :: Int -- location of this tree
@@ -131,11 +216,11 @@ actionFromString s = toAction xs
 readActions n = replicateM n $ do
   actionFromString <$> getLine
 
-cellFromString input_line = Cell {cellIndex=index, cellRichness=richness, cellNeighbours=neighbours}
+cellFromString input_line = Cell index richness neighbours
   where
     input = words input_line
     index = read (input !! 0) :: Int -- 0 is the center cell, the next cells spiral outwards
-    richness = toRichness (read (input !! 1) :: Int) -- 0 if the cell is unusable, 1-3 for usable cells
+    richness = read (input !! 1) :: Int -- 0 if the cell is unusable, 1-3 for usable cells
     neighbours = [read (input !! i) :: Int | i <- [2..7]] -- the index of the neighbouring cell for each direction
 
 readCells n = replicateM n $ do
