5291b71fbe842a48344ed789ed03aa73bd437642
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2022-04-25 21:05:27 +0800

    spring-challenge-2022: bronze

--- src/bot-programming/spring-challenge-2022/spring-challenge-2022.hs
+++ src/bot-programming/spring-challenge-2022/spring-challenge-2022.hs
@@ -9,15 +9,23 @@ import System.IO
       hPutStrLn )
 import Control.Monad ( replicateM, replicateM_ )
 import Debug.Trace ( traceShowM )
+import qualified Data.Map as M
 
 -- Debug helper function to print something
 debug :: (Show a) => [a] -> IO ()
 debug = hPutStrLn stderr . show
 
+mapWidth = 17630
+mapHeight = 9000
+
+type EntityId = Int
 type Coords = (Int, Int)
 
 class HasCoords a where
   coords :: a -> Coords
+
+class HasEntityId a where
+  getId :: a -> EntityId
 
 newtype Base = Base
     { baseCoords :: Coords
@@ -35,7 +43,7 @@ data Entity
 
 data EntityData =
   EntityData
-    { entityId :: Int
+    { entityId :: EntityId
     , entityCoord :: Coords
     , entityShieldLife :: Int
     , entityIsControlled :: Bool
@@ -52,18 +60,26 @@ data MonsterData =
     }
   deriving Show
 
-data ThreatFor = Neutral | MyBase | EnemyBase
-  deriving (Show, Eq, Enum)
-
-data Command = WAIT | MOVE Int Int
-  deriving Show
-
-move = uncurry MOVE . coords
-
 instance HasCoords Entity where
   coords (Monster   e _) = entityCoord e
   coords (MyHero    e)   = entityCoord e
   coords (EnemyHero e)   = entityCoord e
+
+instance HasEntityId Entity where
+  getId (Monster   e _) = entityId e
+  getId (MyHero    e)   = entityId e
+  getId (EnemyHero e)   = entityId e
+
+data ThreatFor = Neutral | MyBase | EnemyBase
+  deriving (Show, Eq, Enum)
+
+data Spell = WIND Int Int | SHIELD EntityId | CONTROL EntityId Int Int
+  deriving Show
+
+data Command = WAIT | MOVE Int Int | SPELL Spell
+  deriving Show
+
+move = uncurry MOVE
 
 distance e1 e2 = distance2d (coords e1) (coords e2)
 
@@ -78,24 +94,40 @@ isMyHero _ = False
 isAThreat (Monster _ md) = monsterThreatFor md == MyBase
 isAThreat _ = False
 
+isMonster (Monster _ _) = True
+isMonster _ = False
+
+defaultPosition i = defaultPositions M.! i
+  where
+    defaultPositions = M.fromList [ (0, (5500, 1500))
+                                  , (1, (4000, 4000))
+                                  , (2, (1500, 5500))
+                                  , (3, (mapWidth - 5500, mapHeight - 1500))
+                                  , (4, (mapWidth - 4000, mapHeight - 4000))
+                                  , (5, (mapWidth - 1500, mapHeight - 5500)) ]
+
 -------------------------------------------------------------------------------
 -- game loop and main logic
 -------------------------------------------------------------------------------
 filterMyHeroes = filter isMyHero
-filterMonsters = filter isAThreat
+filterMonsters = filter isMonster
 
-threatLevel b h e = 17630 - distance b e - distance h e
+threatLevelToBase (Monster _ md) = if monsterThreatFor md == MyBase then 6000*10 else 0
+threatLevelToBase _ = 0
+
+threatLevel b h e = (threatLevelToBase e - distance b e) - distance h e
 
 findHighestThreat b e [x] = x
 findHighestThreat b e (x:xs) = foldr (\o1 o2 -> if threatLevel b e o1 > threatLevel b e o2 then o1 else o2) x xs
 
 nextHeroMove :: Base -> Entity -> [Entity] -> Command
-nextHeroMove b h [] = WAIT
-nextHeroMove b h es = move $ findHighestThreat b h es
+nextHeroMove b h [] = move $ defaultPosition (getId h)
+nextHeroMove b h es = move $ coords $ findHighestThreat b h es
 
--- simplest logic - for each hero find the closes monster who's aiming own base and move to it
+-- ✓ simplest logic - for each hero find the closes monster who's aiming own base and move to it
 -- improvement: separate monster for each hero
--- improvement: do not go far from own base
+-- ✓ improvement: do not go far from own base
+-- ✓ go to the edge of the base
 
 gameLoop :: Base -> Int -> IO ()
 gameLoop base heroesPerPlayer = do
@@ -103,7 +135,7 @@ gameLoop base heroesPerPlayer = do
     [enemyHealth, enemyMana] <- readInts
     entityCount <- readInt
     entities <- replicateM entityCount (makeEntity <$> readInts)
-    -- mapM_ traceShowM entities
+    mapM_ traceShowM entities
     let myHeros = filterMyHeroes entities
     let monsters = filterMonsters entities
 
@@ -117,6 +149,7 @@ main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
     base <- makeBase <$> readInts
+    debug $ show base
     heroesPerPlayer <- readInt
     gameLoop base heroesPerPlayer
 
